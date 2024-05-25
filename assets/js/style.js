7f4314c2d746ca6b33994f48d0678f159557755a
rinklepoonia <rinklepoonia889@gmail.com>
2024-05-25 09:56:59 +0530

    first

---
+++ assets/js/style.js
@@ -0,0 +1,30 @@
+$('.theam_slider').slick({
+    dots: true,
+    // autoplay:true,
+    vertical: true,
+    verticalSwiping: true,
+    infinite: true,
+    arrows: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    // responsive: [
+    //     {
+    //         breakpoint: 992,
+    //         settings: {
+    //             slidesToShow: 4,
+    //             slidesToScroll: 1,
+    //             infinite: true,
+    //             dots: true
+    //         }
+    //     },
+    //     {
+    //         breakpoint: 576,
+    //         settings: {
+    //             slidesToShow: 2,
+    //             slidesToScroll: 1,
+    //             infinite: true,
+    //             dots: true
+    //         }
+    //     },]
+});
